8cf8a248eb6dea20f46d8de646069b0d304656dc
SEBIN SEBASTIAN <mailmesebin00@gmail.com>
2022-04-02 16:38:06 +0000

    Update

--- src/main.c
+++ src/main.c
@@ -10,10 +10,22 @@ int main(int argc, char *argv[])
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
         640, 320, SDL_WINDOW_SHOWN);
-    printf("Hello, World!\n");
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
     while (1)
-    {}
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
+        SDL_Rect r;
+        r.x = 0;
+        r.y = 0;
+        r.w = 40;
+        r.h = 40;
+        SDL_RenderDrawRect(renderer, &r);
+        SDL_RenderPresent(renderer);
+    }
     SDL_DestroyWindow(window);
     return 0;
 }
