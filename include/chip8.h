a8894b89d7bd4246ba68b71d3d27148430f10ef5
SEBIN SEBASTIAN <mailmesebin00@gmail.com>
2022-04-07 13:40:41 +0000

    Create memory structure

--- include/chip8.h
+++ include/chip8.h
@@ -2,10 +2,12 @@
 #define CHIP8_H
 
 #include "config.h"
+#include "chip8memory.h"
 //this is the chip8 structure, anything to do with chip8 internals will go here (memory, registers,keyboard,screen pixels)
 struct chip8
 {
-
+    struct chip8_memory memory;
 };
+
 
 #endif
